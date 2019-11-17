c43df7ac8a8c5d4fbc3714a0f8843c0bfd47c5da
developer1 <chiyochan9@gmail.com>
2019-11-17 08:33:52 -0700

    scm, controller

---
+++ src/main/java/com/example/devops/WelcomeController.java
@@ -0,0 +1,18 @@
+package com.example.devops;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class WelcomeController {
+
+	// @RequestMapping("/")
+	@GetMapping("/")
+	public String welcome(Model model) {
+		model.addAttribute("course", "DevOps");
+		return "index";
+	}
+
+}
